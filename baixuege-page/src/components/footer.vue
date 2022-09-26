a30e6fe2953c62dfbfa8697d3e48428f117a5880
xuwenping <chunguimengliren@163.com>
2022-09-26 15:12:41 +0800

    主页设置备案号

--- baixuege-page/src/components/footer.vue
+++ baixuege-page/src/components/footer.vue
@@ -3,7 +3,7 @@
 <template>
   <footer class="footer">
     <div class="container wrap">
-      <div>京ICP备2021039823号-1</div>
+      <div><a href="https://beian.miit.gov.cn">皖ICP备2021002798号-1</a></div>
       <div>杨柳依依 保留所有权利</div>
       <div>永不言败的心 永不放弃的梦</div>
       <div>源代码 | 邮箱 | 关于</div>
--- baixuege-page/src/views/home/index.vue
+++ baixuege-page/src/views/home/index.vue
@@ -3,6 +3,9 @@
 <template>
   <div class="wrap">
     <h2>得之我幸，失之我命</h2>
+    <div class="beian">
+      <a href="https://beian.miit.gov.cn" target="_blank">皖ICP备2021002798号-1</a>
+    </div>
   </div>
 </template>
 
@@ -23,6 +26,24 @@
     color: #fff;
     text-shadow: 4px 4px 10px #000;
     // transform: translateY(-200px);
+  }
+  .beian {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 36px;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    background-color: #22292d;
+    font-size: 14px;
+    a {
+      color: #666;
+      &:hover{
+        text-decoration: underline;
+      }
+    }
   }
 }
 </style>
